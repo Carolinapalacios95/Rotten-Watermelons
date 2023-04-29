a7ef16ba8a5506010472240e2cbb6344994663d2
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-04-29 14:37:08 -0400

    added missing libraries

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 // below was an example used for the miniproject solved that imports one of the models called project.js
-const Project = require('./Project');
+// const Project = require('./Project');
 
 // below we ill also add aditional tables
 // module.exports = { User, Project }; this is an example
