67f3591017c8a0a7164f3bcf561565b73335d340
Carolina <carolina.palacios1995@gmail.com>
2023-05-01 10:28:46 -0400

    linked to user routes & main.handlebars

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,20 +1,4 @@
 const logout = async () => {
-    const response = await fetch('', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/login');
-    } else {
-      alert(response.statusText);
-    }
-  };
-
-  document.querySelector('#').addEventListener('click', logout);
-// below is the instructional example to assist
-/**
- * const logout = async () => {
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -28,5 +12,3 @@ const logout = async () => {
 };
 
 document.querySelector('#logout').addEventListener('click', logout);
-
- */
