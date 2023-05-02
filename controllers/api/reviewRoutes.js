6e2d35bbe26f96c898e26ef5bcce1adc13bbe6d7
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-05-02 19:33:08 -0400

    added review content

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 const { Review } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/api/reviews',(req,res)=>{
+    
+})
