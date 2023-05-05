ff2bcd953a42388d1221d3b647dec8495a509456
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-05-05 13:25:14 -0400

    testing changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const reviewRoutes = require('./reviewRoutes');
-const movieRoutes = require('./movieRoutes');
 
 router.use('/users', userRoutes);
-router.use('/reviews', reviewRoutes);
-router.use('/movies', movieRoutes);
 
 module.exports = router;
